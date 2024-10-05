f5d9286d3903f4a1368dee303f15713edbbe4ff2
Santiago Fuentes <santigf12@gmail.com>
2024-10-05 03:05:26 -0600

    removed env vars from github image action

--- backend/config/db.js
+++ backend/config/db.js
@@ -8,14 +8,7 @@ let pool;
 
 const connectDB = async () => {
     try {
-
-      console.log({
-        host: process.env.DATABASE_HOST,
-        user: process.env.DATABASE_USER,
-        database: process.env.DATABASE,
-        port: process.env.DATABASE_PORT
-      });
-
+      
       pool = mysql.createPool({
         host: process.env.DATABASE_HOST,
         user: process.env.DATABASE_USER,
