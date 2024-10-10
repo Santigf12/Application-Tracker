e88427fe5e6bfe0ef4fd0541113c82ca6afa8364
Santiago Fuentes <santigf12@gmail.com>
2024-10-10 00:06:09 -0600

    remove minor detail

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,4 +1,3 @@
-import { GoogleOAuthProvider } from '@react-oauth/google';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
@@ -9,13 +8,11 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <GoogleOAuthProvider clientId="930714557596-4rh0j6ugai7a0dp9t9smk02mug1gpb8j.apps.googleusercontent.com">
-    <React.StrictMode>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </React.StrictMode>
-  </GoogleOAuthProvider>
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
