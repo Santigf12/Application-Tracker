468a3a63d5acc61afacfff1f6b43a1f24e596b1a
Santiago Fuentes <santiago@fuentes.it.com>
2026-04-15 02:17:06 -0600

    Minor fix for new backend

--- frontend/src/features/applications/applicationService.ts
+++ frontend/src/features/applications/applicationService.ts
@@ -1,14 +1,13 @@
-import axios from "axios";
+import axios from 'axios';
 
-let API_URL = "";
-if (process.env.NODE_ENV !== "production") {
-  API_URL = "http://localhost:5000/api/applications"; // lets us use the backend server in local development
-}
-else {
-  API_URL = "/api/applications";
+let API_URL = '';
+if (process.env.NODE_ENV !== 'production') {
+  API_URL = 'http://localhost:5000/api/applications/'; // lets us use the backend server in local development
+} else {
+  API_URL = '/api/applications';
 }
 
-import { Application } from "./applicationsSlice";
+import { Application } from './applicationsSlice';
 
 // GET all applications
 const getAllApplications = async (): Promise<Application[]> => {
@@ -26,29 +25,29 @@ const getApplicationById = async (id: string): Promise<Application> => {
 const createApplication = async (application: Application) => {
   const { data } = await axios.post(API_URL, application);
   return data;
-}
+};
 
 //Update application
 const updateApplication = async (id: string, application: Application) => {
-  const { data }  = await axios.put(`${API_URL}/${id}`, application);
+  const { data } = await axios.put(`${API_URL}/${id}`, application);
   return data;
-}
+};
 
 //Delete application
 const deleteApplication = async (id: string) => {
-  const { data }  = await axios.delete(`${API_URL}/${id}`);
+  const { data } = await axios.delete(`${API_URL}/${id}`);
   return data;
-}
+};
 
 const saveCoverLetter = async (id: string, content: string) => {
-  const { data }  = await axios.post(`${API_URL}/${id}`, { content });
+  const { data } = await axios.post(`${API_URL}/${id}`, { content });
   return data;
 };
 
 const getCoverLetter = async (id: string) => {
   const { data } = await axios.get(`${API_URL}/cover-letter/${id}`);
   return data;
-}
+};
 
 const applicationService = {
   getAllApplications,
@@ -57,7 +56,7 @@ const applicationService = {
   updateApplication,
   deleteApplication,
   saveCoverLetter,
-  getCoverLetter
+  getCoverLetter,
 };
 
 export default applicationService;
