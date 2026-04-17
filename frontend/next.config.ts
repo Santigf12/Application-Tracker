2a437c7597b55c61d7c303e487b9baf4421088d7
Santiago Fuentes <santiago@fuentes.it.com>
2026-04-17 16:48:56 -0600

    added api fix v4

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from "next";
 
 const API_URL = process.env.API_URL;
-
 if (!API_URL) {
   throw new Error("Missing API_URL environment variable");
 }
@@ -12,8 +11,12 @@ const nextConfig: NextConfig = {
   async rewrites() {
     return [
       {
+        source: "/api/:path*/",
+        destination: `${API_URL}/api/:path*/`,
+      },
+      {
         source: "/api/:path*",
-        destination: `${API_URL}/api/:path*`,
+        destination: `${API_URL}/api/:path*/`,
       },
     ];
   },
